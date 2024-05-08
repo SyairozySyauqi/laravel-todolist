3c84eda17ae5c083a492efeec646a687a47a31c8
Muhammad Syairozy Syauqi <syairozys@gmail.com>
2024-05-08 10:16:49 +0700

    Membuat Login Action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -12,4 +12,25 @@ class UserControllerTest extends TestCase
     {
         $this->get('/login')->assertSeeText('login');
     }
+
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            'user' => 'MSS',
+            'password' => 'test',
+        ])->assertRedirect('/')->assertSessionHas('user', 'MSS');
+    }
+
+    public function testLoginValidationError()
+    {
+        $this->post('/login', [])->assertSeeText('User or password is required');
+    }
+
+    public function testLoginFailed()
+    {
+        $this->post('/login', [
+            'user'=> 'login',
+            'password'=> 'password',
+        ])->assertSeeText('User or password is wrong');
+    }
 }
